3c6d3adbd9d1b1202dad823438deb8b4b04e51ab
GyuSeong <moon01087209503@gmail.com>
2026-02-05 22:35:43 +0900

    feat : 오브젝트 풀 생성
    탄약 저장할 수 있는 오브젝트 풀 만듬
    개선사항
    1. 적 탄약과 아군탄약 분리
    2. 공격과 연동해 각 무기별 위치에서 발사되는지 확인

---
+++ Assets/Gundam/Game/Script/Base/Managers/ObjectPoolManager.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Base.Manager
+{
+    public enum PoolType
+    {
+        MG_Bullet,
+        MG_Shell,
+        Canon_Bullet,
+        Canon_Shell,
+        SRM,
+        LRM
+    }
+
+    [System.Serializable]
+    public struct PoolData
+    {
+        public GameObject obj;
+        public int count;
+        public PoolType type;
+    }
+
+    /// <summary> PoolData 한 종을 관리하는 클래스</summary>
+    public class ObjectPool
+    {
+        PoolData _data;
+        private GameObject _parent;
+        Queue<GameObject> _pool;
+
+        public ObjectPool(PoolData data, GameObject obj)
+        {
+            _parent = obj;
+            _data = data;
+            _pool = new Queue<GameObject>();
+            AddPool(_data.count);
+        }
+        /// <summary> 풀에 오브젝트 추가 </summary>
+        /// <param name="count">추가할 갯수</param>
+        void AddPool(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                GameObject newObj = GameObject.Instantiate(_data.obj, _parent.transform);
+                newObj.SetActive(false);
+                _pool.Enqueue(newObj);
+            }
+        }
+
+        public GameObject UsePool(Vector3 pos, Quaternion rot)
+        {
+            if (_pool.Count <= 0)
+            {
+                AddPool(_data.count / 3);
+                Debug.Log($"풀 추가 : 현재 {_data.type}의 최대 개수 : {_data.count}");
+            }
+
+            GameObject usingObj = _pool.Dequeue();
+            usingObj.transform.position = pos;
+            usingObj.transform.rotation = rot;
+            return usingObj;
+        }
+
+        public void ReturnPool(GameObject returnedObj)
+        {
+            returnedObj.SetActive(false);
+            _pool.Enqueue(returnedObj);
+        }
+    }
+
+    public class ObjectPoolManager : MonoBehaviour
+    {
+        public List<PoolData> data;
+        public static Dictionary<PoolType, ObjectPool> poolDic;
+
+        private void Awake()
+        {
+            poolDic = new Dictionary<PoolType, ObjectPool>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                AddDictionary(data[i]);
+            }
+            Debug.Log("풀 생성 완료");
+        }
+
+        void AddDictionary(PoolData data)
+        {
+            poolDic.Add(data.type, new ObjectPool(data, gameObject));
+        }
+    }
+}
